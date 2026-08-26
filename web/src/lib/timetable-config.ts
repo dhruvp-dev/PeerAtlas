8b7c62a07e01b83fef4bf5aa859bd96f575bb9a5
dhruv <dhruvpandey20012@gmail.com>
2026-08-26 22:43:36 +0530

    feat: link Timetable PDF to Convex Cloud Storage URL

--- web/src/lib/timetable-config.ts
+++ web/src/lib/timetable-config.ts
@@ -1,6 +1,6 @@
 export const TIMETABLE_PDF_PATH =
   process.env.NEXT_PUBLIC_TIMETABLE_PDF_URL ||
-  "/assets/College of Engineering - Pune Navi Mumbai Campus Theory Timetable Winter - 2026.pdf";
+  "https://groovy-rhinoceros-650.convex.cloud/api/storage/bd04b5cf-1aff-414b-b70b-c779c9e3cea7";
 
 // Exclusive expiry: banner remains visible throughout Nov 30.
 export const TIMETABLE_BANNER_EXPIRY = "2026-12-01";
